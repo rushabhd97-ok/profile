48908252da6affda371c94b228b942f5bdea4933
rushabhd97-ok <rushabh.lucifer@gmail.com>
2026-07-17 11:36:51 +0530

    Code Cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import App from '../App.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <App />
 </template>
