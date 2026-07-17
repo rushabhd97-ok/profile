650f8f38c4b1909ce64496b578d9963d105b3dc1
rushabhd97-ok <rushabh.lucifer@gmail.com>
2026-07-17 12:56:07 +0530

    Check Project

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGitHubPages ? `/profile/` : '/',
   plugins: [
     vue(),
     vueDevTools(),
